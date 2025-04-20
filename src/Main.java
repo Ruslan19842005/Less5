691be4652d2eafd5d58b9e95c33fc1df60532821
yamal <yamal_07@mail.ru>
2025-04-20 20:58:43 +0900

    Zadanie-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
+        int age = 18;
+        if(age <= 18) {
+            System.out.println("Если 18 + age+ можно водить");
+        }
     }
 }
