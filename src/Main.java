82f712e1c1436548d058f3573b2230051d4a811f
yamal <yamal_07@mail.ru>
2025-04-21 00:44:48 +0900

    Zadanie-7

--- src/Main.java
+++ src/Main.java
@@ -61,6 +61,16 @@ public class Main {
             } else {
                 System.out.println("Если в вагоне " + passengers + " пассажиров, то вагон полностью забит");
             }
+
+
+            int one = 5;
+            int two = 10;
+            int three = 7;
+            if (one >= two && one >= three) {
+                System.out.println("Наибольшее число: " + one);
+            } else if (two >= one && two >= three) {
+                System.out.println("Наибольшее число: " + two);
+            }
             }
 
         }
