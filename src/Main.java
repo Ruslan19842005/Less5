0c06eee58fc3dde49e13e917c831f45371498835
yamal <yamal_07@mail.ru>
2025-04-22 00:27:30 +0900

    Zadanie- исправленное

--- src/Main.java
+++ src/Main.java
@@ -3,78 +3,75 @@ public class Main {
 
 
         int age = 18;
-        if (age <= 18) {
-            System.out.println("Если 18 + age + можно водить");
-            if (age < 18) {
-                System.out.println(" Если нет 18 + age + не можешь водить ");
-            }
-
-
-            int temperature = 4;
-            if (temperature < 5) {
-                System.out.println("На улице" + temperature + " градусов,нужно надеть шапку");
-            } else {
-                System.out.println("На улице" + temperature + " градусов,можно идти без шапки");
-            }
-
-
-            int speed = 81;
-            if (speed > 60) {
-                System.out.println(" Если скорость" + speed + "придется платить штраф");
-            } else {
-                System.out.println("Если скорость" + speed + "штрафа не будет");
-            }
-
-            int age1 = 2;
-            if (age1 >= 2 && age1 <= 6) {
-                System.out.println("Если возраст равен" + age1 + " ему нужно ходить в садик");
-            } else if (age1 >= 7 && age1 <= 17) {
-                System.out.println("Если возраст равен" + age1 + "ему нужно ходить в школу");
-            } else if (age1 >= 18 && age1 < +24) {
-                System.out.println("Если возраст равен" + age1 + "ему нужно ходить в институт");
-            } else {
-                System.out.println("Если возраст равен" + age1 + " ему нужно ходить на работу");
-            }
-
-
-            int ageThree = 7;
-            boolean adult = true;
-            if (ageThree < 5) {
-                System.out.println("Если возраст ребенка " + ageThree + " он не может кататься");
-            } else if (ageThree >= 5 && ageThree < 14) {
-                if (adult) {
-                    System.out.println("Если возраст ребенка" + ageThree + " только в сопровождении взрослых");
-                } else {
-                    System.out.println("Если возраст ребенка " + ageThree + " нельзя кататься без сопровождения взрослых");
-                }
-            } else {
-                System.out.println("Если возраст ребенка" + ageThree + "можно кататься без сопровождения взрослого");
-            }
-
-
-
-            int passengers = 75;
-            if (passengers <= 60) {
-                System.out.println("Если в вагоне " + passengers + " пассажиров, то есть сидячие места");
-            } else if (passengers <= 102) {
-                System.out.println("Если в вагоне " + passengers + " пассажиров, то есть стоячие места");
-            } else {
-                System.out.println("Если в вагоне " + passengers + " пассажиров, то вагон полностью забит");
-            }
-
-
-            int one = 5;
-            int two = 10;
-            int three = 7;
-            if (one >= two && one >= three) {
-                System.out.println("Наибольшее число: " + one);
-            } else if (two >= one && two >= three) {
-                System.out.println("Наибольшее число: " + two);
-            }
-            }
-
+        if (age >= 18) {
+            System.out.println("Если " + age + " можно водить");
         }
+        if (age < 18) {
+            System.out.println(" Если возраст " + age + " не можешь водить ");
+        }
+
+
+        int temperature = 4;
+        if (temperature < 5) {
+            System.out.println("На улице" + temperature + " градусов,нужно надеть шапку");
+        } else {
+            System.out.println("На улице" + temperature + " градусов,можно идти без шапки");
+        }
+
+
+        int speed = 81;
+        if (speed > 60) {
+            System.out.println(" Если скорость" + speed + "придется платить штраф");
+        } else {
+            System.out.println("Если скорость" + speed + "штрафа не будет");
+        }
+
+
+        if (age >= 2 && age <= 6) {
+            System.out.println("Если возраст равен" + age + " ему нужно ходить в садик");
+        } else if (age >= 7 && age <= 17) {
+            System.out.println("Если возраст равен" + age + "ему нужно ходить в школу");
+        } else if (age >= 18 && age < +24) {
+            System.out.println("Если возраст равен" + age + "ему нужно ходить в институт");
+        } else {
+            System.out.println("Если возраст равен" + age + " ему нужно ходить на работу");
+        }
+
+
+        if (age < 5) {
+            System.out.println("Если возраст ребенка " + age + " он не может кататься");
+        } else if (age >= 5 && age < 14) {
+            System.out.println("Если возраст ребенка" + age + " только в сопровождении взрослых");
+        } else {
+            System.out.println("Если возраст ребенка" + age + "можно кататься без сопровождения взрослого");
+        }
+
+
+        int passengers = 75;
+        if (passengers <= 60) {
+            System.out.println("Если в вагоне " + passengers + " пассажиров, то есть сидячие места");
+        } else if (passengers <= 102) {
+            System.out.println("Если в вагоне " + passengers + " пассажиров, то есть стоячие места");
+        } else {
+            System.out.println("Если в вагоне " + passengers + " пассажиров, то вагон полностью забит");
+        }
+
+
+        int one = 5;
+        int two = 10;
+        int three = 7;
+        if (one >= two && one >= three) {
+            System.out.println("Наибольшее число: " + one);
+        } else if (two >= one && two >= three) {
+            System.out.println("Наибольшее число: " + two);
+        } else {
+            System.out.println("Наибольшее число " + three);
+        }
+
     }
+
+}
+
 
 
 
